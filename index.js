e21a1625ab58cb452d1fc88029d79eb226e2839a
Tulasi L <79722173+tulasi3231@users.noreply.github.com>
2023-07-25 13:21:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,87 +1,81 @@
-const PORT = process.env.PORT || 8000  //to deploy on heroku
-const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
+const PORT = process.env.PORT || 8000; //to deploy on Heroku
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-const app = express()
+const app = express();
 
 const newspapers = [
     {
         name: 'theTimes',
-        address:'https://www.thetimes.co.uk/environment/climate-change',
-        base:''
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
     },
     {
         name: 'guardian',
-        address:'https://www.theguardian.com/environment/climate-crisis',
-        base:''
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: ''
     },
     {
         name: 'telegraph',
-        address:'https://www.telegraph.co.uk/climate-change',
-        base:'https://www.telegraph.co.uk'
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk'
     }
+];
 
-]
-
-
-const articles = []
+const articles = [];
 
 newspapers.forEach(newspaper => {
     axios.get(newspaper.address)
         .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
+            const html = response.data;
+            const $ = cheerio.load(html);
 
             $('a:contains("climate")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
+                const title = $(this).text().replace(/[\t\n]/g, ''); // Removing \t and \n characters from title
+                const url = $(this).attr('href');
                 articles.push({
                     source: newspaper.name,
                     title,
-                    url:newspaper.base + url
-
-                })
-
-            })
+                    url: newspaper.base + url
+                });
+            });
 
         })
-})
+        .catch(err => console.error(err)); // Handle errors in the Axios request
+});
 
-app.get('/',(req,res) => {
-    res.json('Welcome to Climate Change News API')
-})
+app.get('/', (req, res) => {
+    res.json('Welcome to Climate Change News API');
+});
 
+app.get('/news', (req, res) => {
+    res.json(articles);
+});
 
-app.get('/news', (req,res) =>{
-    res.json(articles)
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId;
 
-})
-
-app.get('/news/:newspaperId', (req,res) => {
-    const newspaperId = req.params.newspaperId
-
-    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
-    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address;
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base;
     axios.get(newspaperAddress)
-        .then(response =>
-        {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const specificArticles =[]
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const specificArticles = [];
 
-            $('a:contains("climate")',html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text().replace(/[\t\n]/g, ''); // Removing \t and \n characters from title
+                const url = $(this).attr('href');
                 specificArticles.push({
-                    source : newspaperId,
+                    source: newspaperId,
                     title,
                     url: newspaperBase + url
+                });
+            });
+            res.json(specificArticles);
+        })
+        .catch(err => console.log(err)); // Handle errors in the Axios request
+});
 
-                })
-            })
-            res.json(specificArticles)
-        }).catch(err => console.log(err))
-
-})
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
